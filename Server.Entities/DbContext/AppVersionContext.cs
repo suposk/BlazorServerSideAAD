d3abac5b2f3a0146dd68409b81904e3632bc0f7b
suposk <jan.supolik@gmail.com>
2020-12-03 11:31:19 +0100

    using local db sql lite

--- Server.Entities/DbContext/AppVersionContext.cs
+++ Server.Entities/DbContext/AppVersionContext.cs
@@ -1,41 +1,61 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Text;
-//using Microsoft.EntityFrameworkCore;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-//namespace Server.Entities.DbContext
-//{
 
-//    public class AppVersionContext : DbContext
-//    {
-//        private readonly string _connectionString;
+namespace Server.Entities.DbContext
+{
 
-//        public AppVersionContext() : base()
-//        {
-//            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
-//        }
-//        public AppVersionContext(string connectionString) : base()
-//        {
-//            _connectionString = connectionString;
-//            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
-//        }
+    public class AppVersionContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        private readonly string _connectionString;
 
-//        public PdeContext(DbContextOptions<AppVersionContext> options)
-//           : base(options)
-//        {
-//            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
-//        }
+        public AppVersionContext() : base()
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        }
+        public AppVersionContext(string connectionString) : base()
+        {
+            _connectionString = connectionString;
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        }
 
-//        public DbSet<AppVersion> AppVersions { get; set; }
+        public AppVersionContext(DbContextOptions<AppVersionContext> options)
+           : base(options)
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        }
 
-//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-//        {
-//            if (!optionsBuilder.IsConfigured)
-//            {
-//                //sql Lite
-//                if (!string.IsNullOrWhiteSpace(_connectionString))
-//                    optionsBuilder.UseSqlite(_connectionString);
-//            }
-//        }
-//    }
-//}
+        public DbSet<AppVersion> AppVersions { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                //sql Lite
+                if (!string.IsNullOrWhiteSpace(_connectionString))
+                {
+                    //optionsBuilder.UseSqlite(_connectionString);
+                }
+            }
+
+            //optionsBuilder.Entity<AppVersion>().HasData(
+            //    new AppVersion()
+            //    {
+            //        Id = 22, 
+            //        CreatedAt = 
+            //    },
+            //    new AppVersion()
+            //    {
+            //        AddressId = 2,
+            //        Address1 = "Ulica 22",
+            //        City = "Trstena",
+            //        Country = "SK",
+            //        PostalCode = "11222",
+            //        StateProvince = "ZA"
+            //    });
+        }
+    }
+}
--- WebApiNetCore3/Program.cs
+++ WebApiNetCore3/Program.cs
@@ -1,7 +1,10 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Server.Entities.DbContext;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +16,36 @@ namespace WebApiNetCore3
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            //CreateHostBuilder(args).Build().Run();
+
+            var host = CreateHostBuilder(args).Build();
+
+            // migrate the database.  Best practice = in Main, using service scope
+            using (var scope = host.Services.CreateScope())
+            {
+                ILogger<Program> logger = null;
+                try
+                {
+                    logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+                }
+                catch { }
+
+                try
+                {
+                    logger?.LogInformation("CreateHostBuilder Started");
+
+                    var context = scope.ServiceProvider.GetService<AppVersionContext>();
+
+                    // for demo purposes, delete the database & migrate on startup so we can start with a clean slate                   
+                    //context.Database.EnsureDeleted(); logger?.LogInformation("Called EnsureDeleted");
+                    context.Database.EnsureCreated(); logger?.LogInformation("Called EnsureCreated");
+                    context.Database.Migrate(); logger?.LogInformation("Called Migrate");
+                }
+                catch (Exception ex)
+                {
+                    logger?.LogError(ex, "An error occurred while migrating the database.");
+                }
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
